186fcb6edc5ee8fcf36014b951b8f70d877e9828
Moustapha76 <moustapha.md26@gmail.com>
2023-08-22 13:25:11 +0200

    single page

--- js/main.js
+++ js/main.js
@@ -2,3 +2,23 @@ window.addEventListener('scroll', function(e) {
     var navbar = document.querySelector('nav')
     if (this.scrollY > 0) { navbar.classList.add('fixed') } else { navbar.classList.remove('fixed') }
 })
+
+$(function() {
+    var filterList = {
+        init: function() {
+            // MixItUp plugin
+            // http://mixitup.io
+            $('.item-grid').mixItUp({
+                selectors: {
+                    target: '.item',
+                    filter: '.filter'
+                },
+                load: {
+                    filter: 'all' // show app tab on first load
+                }
+            });
+        }
+    };
+    // Run the show!
+    filterList.init();
+});
